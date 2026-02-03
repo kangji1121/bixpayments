f0328bd3e5879b2cb41a8ca322dc75ae1e754206
강지현 <wlgus4083@gmail.com>
2026-02-03 18:50:17 +0900

    feat: restrict routes to login/signup when unauthenticated

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import BoardList from "../pages/BoardList";
@@ -6,24 +6,34 @@ import BoardDetail from "../pages/BoardDetail";
 import BoardCreate from "../pages/BoardCreate";
 import BoardEdit from "../pages/BoardEdit";
 import RequireAuth from "./RequireAuth";
+import useAuthStore from "../store/authStore";
+
+// 로그인 안 했을 때만 /login, /signup 접근 허용
+function PublicOnly() {
+  const accessToken = useAuthStore((s) => s.accessToken);
+  return accessToken ? <Navigate to="/boards" replace /> : <Outlet />;
+}
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/boards" replace />} />
+      {/* 비로그인: 로그인/회원가입만 노출 */}
+      <Route element={<PublicOnly />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-
-      <Route path="/boards" element={<BoardList />} />
-      <Route path="/boards/:id" element={<BoardDetail />} />
-
+      {/* 로그인: 나머지 모든 페이지 접근 가능 */}
       <Route element={<RequireAuth />}>
+        <Route path="/" element={<Navigate to="/boards" replace />} />
+        <Route path="/boards" element={<BoardList />} />
+        <Route path="/boards/:id" element={<BoardDetail />} />
         <Route path="/boards/new" element={<BoardCreate />} />
         <Route path="/boards/:id/edit" element={<BoardEdit />} />
       </Route>
 
-      <Route path="*" element={<div>404</div>} />
+      {/* 그 외 모든 경로는 로그인으로 */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
